783a09e5fb889d9ffed51df58a90f5d685bdecd1
Philani Ndlovu <philani.software.dev@gmail.com>
2022-01-25 17:57:48 +0200

    calendarjs_plugin
    calender.io and popup modal

---
+++ ApplicationScheduling/ApplicationScheduling/Controllers/AppointmentController.cs
@@ -0,0 +1,29 @@
+﻿using ApplicationScheduling.Services;
+using ApplicationScheduling.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationScheduling.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        private readonly IAppointmentService _appointmentService;
+
+        public AppointmentController(IAppointmentService appointmentService)
+        {
+            _appointmentService = appointmentService;
+        }
+
+      
+        public IActionResult Index()
+        {
+            ViewBag.Duration = Helper.GetTimeDropDown();
+            ViewBag.DoctorList = _appointmentService.GetDoctorList();
+            ViewBag.PatientList = _appointmentService.GetPatientList();
+            return View();
+        }
+    }
+}
